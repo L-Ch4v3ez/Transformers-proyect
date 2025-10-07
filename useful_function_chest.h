711839c66ce6bfa8390edda80022139c1a36af02
Leyla Elizabeth T <tamboleyla9@gmail.com>
2025-10-06 21:27:59 -0400

    Add files via upload

--- useful_function_chest.h
+++ useful_function_chest.h
@@ -134,6 +134,38 @@ void mostrar_transformer (Transformers bot){
     mostrar_fecha(bot.creacion);
 }
 
+void ordenar_fecha(Transformers bot[], int n){
+    for (int i = 1; i < n; ++i) {
+         Transformers key = bot[i];
+         int j = i - 1;
+         while (j >= 0 && bot[j].creacion.day > key.creacion.day) {
+              bot[j + 1] = bot[j];
+              j= j - 1;
+         }
+         bot[j + 1] = key;
+    }
+
+    for (int i = 1; i < n; ++i) {
+        Transformers key = bot[i];
+        int j = i - 1;
+        while (j >= 0 && bot[j].creacion.month > key.creacion.month) {
+            bot[j + 1] = bot[j];
+            j = j - 1;
+        }
+        bot[j + 1] = key;
+    }
+
+    for (int i = 1; i < n; ++i) {
+        Transformers key = bot[i];
+        int j = i - 1;
+        while (j >= 0 && bot[j].creacion.year > key.creacion.year) {
+            bot[j + 1] = bot[j];
+            j = j - 1;
+        }
+        bot[j + 1] = key;
+    }
+}
+
 void modificar_bot(Transformers &bot) {
     int opcion;
     do {
@@ -178,6 +210,58 @@ void modificar_bot(Transformers &bot) {
     } while(opcion != 6);
 }
 
+void agrupar_decepticons_autobots (Transformers transformers[], int &bot_num){
+    cout << "Autobots: \n";
+    for (int i=0; i<bot_num; i++){
+        if ( transformers[i].tipo == "Autobot")
+        {
+            cout << transformers[i].designacion << "\n";
+        }
+    }
+    cout << endl;
+    cout << "Decepticons: \n";
+    for (int i=0; i<bot_num; i++){
+        if ( transformers[i].tipo == "Decepticon")
+        {
+            cout << transformers[i].designacion << "\n";
+        }
+    }
+}
+
+void promedio_fuerza (Transformers transformers [], int &bot_num){
+    int prom_autobot= 0;
+    int sum_autobot= 0;
+    int prom_decepticon =  0;
+    int sum_decepticon = 0;
+    for (int i=0; i<bot_num;i++){
+        if (transformers[i].tipo == "Autobot"){
+            sum_autobot++;
+            prom_autobot = prom_autobot + transformers[i].stats.strenght ;
+        }
+        if (transformers[i].tipo == "Decepticon"){
+            sum_decepticon++;
+            prom_decepticon = prom_decepticon + transformers[i].stats.strenght ;
+        }
+    }
+    prom_autobot = prom_autobot/sum_autobot;
+    prom_decepticon = prom_decepticon/sum_decepticon;
+    cout << "El promedio de fuerza de los Autobots es de: " << prom_autobot << endl;
+    cout << "El promedio de fuerza de los Decepticons es de: " << prom_decepticon <<endl;
+}
+
+void fight(Transformers bots[], Transformers bot_1, Transformers bot_2){
+     int prob_bot1=(bot_1.stats.strenght +bot_1.stats.speed +bot_1.stats.intelligence +bot_1.stats.firepower) * bot_1.stats.endurance;
+     int prob_bot2=(bot_2.stats.strenght +bot_2.stats.speed +bot_2.stats.intelligence +bot_2.stats.firepower) * bot_2.stats.endurance;
+     int total = prob_bot1+prob_bot2;
+     int r = rand() % total+1;
+     cout << r<<endl;
+     this_thread::sleep_for(std::chrono::milliseconds(300));
+     if(r <= prob_bot1){
+        cout <<"Ganador: "<< bot_1.designacion<<endl;
+     } else {
+        cout <<"Ganador: "<< bot_2.designacion<<endl;
+     }}
+
 void menu(Transformers transformers[], int &bot_num) {
     int option = 0;
     do {
@@ -185,8 +269,11 @@ void menu(Transformers transformers[], int &bot_num) {
         cout << "1. Registrar un bot\n";
         cout << "2. Info de un bot\n";
         cout << "3. FIGHT\n";
-        cout << "4. Modificar bot \n";
-        cout << "5. Power Down (Exit) \n";
+        cout << "4. Ordenar por fecha\n";
+        cout << "5. Modificar bot \n";
+        cout << "6. Agrupar Autobots y Decepticons \n";
+        cout << "7. Promedio de fuerza \n";
+        cout << "8. Power Down (Exit) \n";
         cout << "Elige opcion: ";
         cin >> option;
         cin.ignore();
@@ -223,10 +310,25 @@ void menu(Transformers transformers[], int &bot_num) {
             break;
         }
         case 3:{
+        Transformers bot_1, bot_2;
+            cout << "Choose Fighter 1:"<<endl;
+            getline(cin,bot_1.designacion);
+            int pos_bot_1=searching_string(transformers, bot_1.designacion,bot_num);
+            cout << "Choose Fighter 2:"<<endl;
+            getline(cin,bot_2.designacion);
+            int pos_bot_2=searching_string(transformers, bot_2.designacion,bot_num);
+            cout << bot_1.designacion << " vs " <<bot_2.designacion<<endl;
+            fight(transformers,transformers[pos_bot_1],transformers[pos_bot_2]);
+            break;
 
+}
+        case 4:{
+            ordenar_fecha(transformers,bot_num);
+            mostrar_transformers(transformers, bot_num);
+            break;
             }
 
-        case 4: {
+        case 5: {
             ordenar_nombre(transformers, bot_num);
             string to_find;
             cout << "Designacion del bot a modificar: ";
@@ -235,18 +337,27 @@ void menu(Transformers transformers[], int &bot_num) {
             int pos = searching_string(transformers, to_find, bot_num);
             if (pos > -1) {
             modificar_bot(transformers[pos]);
-    } else {
-        cout << "No existe ese bot.\n";
-    }
-    break;
-}
-        case 5:
+            } else
+            {
+            cout << "No existe ese bot.\n";
+            }
+            break;
+            }
+        case 6:{
+            agrupar_decepticons_autobots(transformers,bot_num);
+            break;
+        }
+        case 7: {
+            promedio_fuerza(transformers, bot_num);
+            break;
+        }
+        case 8:
             cout << "Saliendo del menu...\n";
             break;
         default:
             cout << "Opcion invalida, intenta otra vez. :( \n";
         }
-    } while(option != 5);
+    } while(option != 8);
 }
 
 #endif // USEFUL_FUNCTION_CHEST_H_INCLUDED
